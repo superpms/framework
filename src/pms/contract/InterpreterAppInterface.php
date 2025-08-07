da5326791b80c90b7592eee4622e7934bbc8cc1a
Guest <kid666@88.com>
2025-08-07 19:40:45 +0800

    修复BUG

--- src/pms/contract/InterpreterAppInterface.php
+++ src/pms/contract/InterpreterAppInterface.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace pms\contract;
-use pms\program\boot\Options;
 
 interface InterpreterAppInterface{
 
     /**
      * @return mixed|null 应用入口函数
      */
-    public static function run(Options $bootOptions): mixed;
+    public static function run(\pms\program\boot\Options $bootOptions): mixed;
 }
