2bee385f617f451159fc4978b17446f91780513b
Guest <kid666@88.com>
2025-10-31 04:41:33 +0800

    增加容器型options类

---
+++ src/pms/cntrOptions.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace pms;
+
+class cntrOptions extends ArrayObjectAccess
+{
+    /**
+     * @throws \Exception
+     */
+    public function __get(string $name)
+    {
+        return $this->data[$name] ?? throw new \Exception("no such option: $name");
+    }
+
+    public function __set(string $name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __isset(string $name): bool
+    {
+        return isset($this->data[$name]);
+    }
+
+    public function __construct(){
+        $tmp = get_object_vars($this);
+        unset($tmp['data']);
+        $this->data = [
+            ...$this->data,
+            ...$tmp
+        ];
+    }
+}
