47b412348a433e6a499c73e699ea4b326c7b6960
Guest <kid666@88.com>
2025-11-03 18:37:47 +0800

    增加标准生命周期

--- bin/const.php
+++ bin/const.php
@@ -12,4 +12,33 @@ const XML_CONTENT_TYPE = 'text/xml;charset=utf-8;';
 const PNG_CONTENT_TYPE = 'image/png';
 const JPEG_CONTENT_TYPE = 'image/jpeg';
 const MPEG_CONTENT_TYPE = 'audio/mpeg';
+
+/**
+ * Lifecycle：启动
+ */
 const LIFECYCLE_BOOT = 'LIFECYCLE_BOOT';
+
+/**
+ * Lifecycle：启动完成
+ */
+const LIFECYCLE_BOOTED = 'LIFECYCLE_BOOTED';
+
+/**
+ * Lifecycle：服务启动完成
+ */
+const LIFECYCLE_SERVER_BOOTED = 'LIFECYCLE_SERVER_BOOTED';
+
+/**
+ * Lifecycle：沙盒启动完成
+ */
+const LIFECYCLE_SANDBOX_BOOTED = 'LIFECYCLE_SANDBOX_BOOTED';
+
+/**
+ * Lifecycle：沙盒执行结束
+ */
+const LIFECYCLE_SANDBOX_RAN = 'LIFECYCLE_SANDBOX_RAN';
+
+/**
+ * Lifecycle：沙盒销毁
+ */
+const LIFECYCLE_SANDBOX_DESTRUCT = 'LIFECYCLE_SANDBOX_DESTRUCT';
