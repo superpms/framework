69649b27c4013dc37374bd17f4b5f61569a6b950
Guest <kid666@88.com>
2025-11-27 07:45:32 +0800

    增加value_compare方法

--- bin/helper.php
+++ bin/helper.php
@@ -3,15 +3,13 @@
 use pms\facade\Path;
 
 if (!function_exists('custom_error_handler')) {
-    function custom_error_handler($errno, $errstr, $errfile, int $errline)
-    {
+    function custom_error_handler($errno, $errstr, $errfile, int $errline) {
         throw new \pms\exception\WarningException($errno, $errstr, $errfile, $errline);
     }
 }
 
 if (!function_exists('dd')) {
-    function dd(mixed ...$vars): void
-    {
+    function dd(mixed ...$vars): void {
         if (!in_console() && !headers_sent()) {
             header('HTTP/1.1 500 Internal Server Error');
             header('Content-Type: text/html');
@@ -35,70 +33,64 @@ if (!function_exists('json_validate')) {
     /**
      * 判断是否为有效json数据
      *
-     * @param string $string 数据
+     * @param  string  $string  数据
      * @return bool
      */
-    function json_validate(string $string): bool
-    {
+    function json_validate(string $string): bool {
         json_decode($string);
         return json_last_error() === JSON_ERROR_NONE;
     }
 }
 
 if (!function_exists('in_console')) {
-    function in_console(): bool
-    {
+    function in_console(): bool {
         return in_array(PHP_SAPI, ['cli', 'phpdbg', 'embed'], true);
     }
 }
 if (!function_exists('in_swoole')) {
-    function in_swoole(): bool
-    {
+    function in_swoole(): bool {
         return defined('SWOOLE_VERSION') && SWOOLE_VERSION !== null;
     }
 }
 
 if (!function_exists('config')) {
-    function config(string $name = null, $default = null)
-    {
+    function config(string $name = null, $default = null) {
         return \pms\facade\Config::get($name, $default);
     }
 }
 if (!function_exists('is_dev')) {
-    function is_dev(): bool
-    {
+    function is_dev(): bool {
         return file_exists(Path::getRoot("/dev.lock"));
     }
 }
 
-if(!defined('DIRECTORY_SEPARATOR_REVERSE')){
-	define('DIRECTORY_SEPARATOR_REVERSE', DIRECTORY_SEPARATOR === '/' ? '\\' : '/');
+if (!defined('DIRECTORY_SEPARATOR_REVERSE')) {
+    define('DIRECTORY_SEPARATOR_REVERSE', DIRECTORY_SEPARATOR === '/' ? '\\' : '/');
 }
 
 if (!function_exists('path_join')) {
-    function path_join(...$segments): string
-    {
+    function path_join(...$segments): string {
         $segments = array_filter($segments);
-        $path = array_map(function ($segment){
+        $path = array_map(function ($segment) {
             if (is_array($segment)) {
                 $segment = path_join(...$segment);
             }
             return str_replace(DIRECTORY_SEPARATOR_REVERSE, DIRECTORY_SEPARATOR, $segment);
         }, $segments);
-
+        
         $parts = explode(DIRECTORY_SEPARATOR, join(DIRECTORY_SEPARATOR, $path));
         $stack = [];
-
+        
         foreach ($parts as $key => $part) {
             // 忽略空段和当前目录
             if (($key !== 0 && $part === '') || $part === '.') continue;
-
+            
             // 处理上级目录
             if ($part === '..') {
                 if (!empty($stack)) array_pop($stack);
                 continue;
             }
-
+            
             $stack[] = $part;
         }
         // 重新组合路径
@@ -106,16 +98,14 @@ if (!function_exists('path_join')) {
     }
 }
 if (!function_exists('path_class')) {
-    function path_class(string $path): string
-    {
+    function path_class(string $path): string {
         $path = str_replace(DIRECTORY_SEPARATOR, '\\', $path);
         return str_replace('.php', '', $path);
     }
 }
 
 if (!function_exists('array_chain')) {
-    function array_chain(array $data, string $chain, string $chainLevelStr = '.')
-    {
+    function array_chain(array $data, string $chain, string $chainLevelStr = '.') {
         $tmp = $data;
         $fA = explode($chainLevelStr, $chain);
         for ($i = 0; $i < count($fA); $i++) {
@@ -130,11 +120,10 @@ if (!function_exists('array_chain')) {
     }
 }
 if (!function_exists('array_chain_set')) {
-    function array_chain_set(array &$data, string $chain, mixed $value, string $chainLevelStr = '.'): void
-    {
+    function array_chain_set(array &$data, string $chain, mixed $value, string $chainLevelStr = '.'): void {
         $result = &$data;
         $current = &$result;
-
+        
         $fA = explode($chainLevelStr, $chain);
         if (count($fA) === 1) {
             $result[$chain] = $value;
@@ -158,10 +147,8 @@ if (!function_exists('array_chain_set')) {
     }
 }
 if (!function_exists('array_to_xml')) {
-    function array_to_xml(array|object $array, string $root = 'root'): bool|string
-    {
-        function arrayToXml($array, &$xml): void
-        {
+    function array_to_xml(array|object $array, string $root = 'root'): bool|string {
+        function arrayToXml($array, &$xml): void {
             foreach ($array as $key => $value) {
                 if (is_array($value) || is_object($value)) {
                     if (!is_numeric($key)) {
@@ -175,15 +162,14 @@ if (!function_exists('array_to_xml')) {
                 }
             }
         }
-
+        
         $xml = new \SimpleXMLElement('<' . $root . '/>');
         arrayToXml($array, $xml);
         return $xml->saveXML();
     }
 }
 if (!function_exists('array_merge_deep')) {
-    function array_merge_deep(...$args): array
-    {
+    function array_merge_deep(...$args): array {
         $result = [];
         foreach ($args as $array) {
             if (is_array($array)) {
@@ -204,20 +190,18 @@ if (!function_exists('array_merge_deep')) {
 
 
 if (!function_exists('object_chain')) {
-    function object_chain(array $data, string $chain, string $chainLevelStr = '.')
-    {
+    function object_chain(array $data, string $chain, string $chainLevelStr = '.') {
         return array_chain($data, $chain, $chainLevelStr);
     }
 }
 if (!function_exists('object_chain_set')) {
-    function object_chain_set(array &$data, string $chain, mixed $value, string $chainLevelStr = '.'): void{
+    function object_chain_set(array &$data, string $chain, mixed $value, string $chainLevelStr = '.'): void {
         array_chain_set($data, $chain, $chainLevelStr);
     }
 }
 
 if (!function_exists('config_load_php')) {
-    function config_load_php(string $file)
-    {
+    function config_load_php(string $file) {
         if (is_file($file)) {
             return include $file;
         }
@@ -225,14 +209,13 @@ if (!function_exists('config_load_php')) {
     }
 }
 if (!function_exists('config_load_ini')) {
-    function config_load_ini(string $file): array
-    {
+    function config_load_ini(string $file): array {
         if (!is_file($file)) {
             return [];
         }
         $file = file_get_contents($file);
         $fileArr = explode("\r\n", $file);
-
+        
         $fileStr = "";
         foreach ($fileArr as $value) {
             if (str_starts_with($value, '#')) {
@@ -240,9 +223,9 @@ if (!function_exists('config_load_ini')) {
             }
             $fileStr .= $value . "\r\n";
         }
-
+        
         $info = parse_ini_string($fileStr, true, INI_SCANNER_TYPED);
-
+        
         if ($info === false) {
             return [];
         }
@@ -271,8 +254,7 @@ if (!function_exists('config_load_ini')) {
     }
 }
 if (!function_exists('load_file_config')) {
-    function load_file_config(string|array $filePath): array
-    {
+    function load_file_config(string|array $filePath): array {
         if (is_string($filePath)) {
             $filePath = [$filePath];
         }
@@ -314,8 +296,7 @@ if (!function_exists('load_file_config')) {
 }
 
 if (!function_exists('str_to_fn')) {
-    function str_to_fn(string|array $fnName, $value): mixed
-    {
+    function str_to_fn(string|array $fnName, $value): mixed {
         if (is_array($fnName)) {
             $fnName = join('|', $fnName);
         }
@@ -393,12 +374,11 @@ if (!function_exists('str_to_fn')) {
 if (!function_exists('bit_or')) {
     /**
      * 分离或运算和值
-     * @param array $keyMap 或运算索引表(所有能进行或运算的原子值集合)[1,2,4,...]
-     * @param int $value 和值(通过索引表中包含的数进行的任意或运算)
+     * @param  array  $keyMap  或运算索引表(所有能进行或运算的原子值集合)[1,2,4,...]
+     * @param  int    $value   和值(通过索引表中包含的数进行的任意或运算)
      * @return array
      */
-    function bit_or(array $keyMap, int $value): array
-    {
+    function bit_or(array $keyMap, int $value): array {
         $result = [];
         foreach ($keyMap as $v) {
             if (($value & $v) === $v) {
@@ -412,12 +392,11 @@ if (!function_exists('bit_or')) {
 if (!function_exists('valid_datatype_or')) {
     /**
      * 验证数据类型(或)
-     * @param string|array $type 数据类型(传入数组 或 用 | 分割多个类型)
-     * @param mixed $datum 数据
+     * @param  string|array  $type   数据类型(传入数组 或 用 | 分割多个类型)
+     * @param  mixed         $datum  数据
      * @return bool
      */
-    function valid_datatype_or(string|array $type, mixed $datum): bool
-    {
+    function valid_datatype_or(string|array $type, mixed $datum): bool {
         try {
             if (is_array($type)) {
                 $type = join('|', $type);
@@ -467,12 +446,11 @@ if (!function_exists('valid_datatype_or')) {
 if (!function_exists('dir_create')) {
     /**
      * 创建文件夹
-     * @param string $path 文件夹地址
-     * @param int $permissions 权限, 默认0777
+     * @param  string  $path         文件夹地址
+     * @param  int     $permissions  权限, 默认0777
      * @return bool
      */
-    function dir_create(string $path, int $permissions = 0777): bool
-    {
+    function dir_create(string $path, int $permissions = 0777): bool {
         if (file_exists($path)) {
             return false;
         }
@@ -483,16 +461,15 @@ if (!function_exists('dir_create')) {
 if (!function_exists('file_create')) {
     /**
      * 创建文件
-     * @param string $path 文件地址
-     * @param string $data 文件数据
-     * @param string $mode 文件模式, 默认w，可选w,a,,x,
-     * ‘w'：创建文件。如果文件存在则覆盖内容。
-     * ‘a'：创建文件。如果文件存在则追加内容。
-     * ‘x'：创建文件，在文件不存在时才创建。
+     * @param  string  $path  文件地址
+     * @param  string  $data  文件数据
+     * @param  string  $mode  文件模式, 默认w，可选w,a,,x,
+     *                        ‘w'：创建文件。如果文件存在则覆盖内容。
+     *                        ‘a'：创建文件。如果文件存在则追加内容。
+     *                        ‘x'：创建文件，在文件不存在时才创建。
      * @return bool
      */
-    function file_create(string $path, string $data, string $mode = "w"): bool
-    {
+    function file_create(string $path, string $data, string $mode = "w"): bool {
         dir_create(pathinfo($path)['dirname']);
         $file = fopen($path, $mode);
         if (!$file) {
@@ -506,13 +483,12 @@ if (!function_exists('file_create')) {
 
 if (!function_exists('class_annotate_attrs')) {
     /**
-     * @param ReflectionClass $class
-     * @param string $name
-     * @param bool $final 是否只返回最后一项
+     * @param  ReflectionClass  $class
+     * @param  string           $name
+     * @param  bool             $final  是否只返回最后一项
      * @return ReflectionAttribute|ReflectionAttribute[]
      */
-    function class_annotate_attrs(ReflectionClass $class, string $name, bool $final = false): array|ReflectionAttribute
-    {
+    function class_annotate_attrs(ReflectionClass $class, string $name, bool $final = false): array|ReflectionAttribute {
         // 获取当前类的属性
         $attrs = $class->getAttributes($name);
         if ($final && !empty($attrs)) {
@@ -530,7 +506,7 @@ if (!function_exists('class_annotate_attrs')) {
                 return $attrs[count($attrs) - 1];
             }
         }
-
+        
         $class = $class->getParentClass();
         if ($class) {
             $child = class_annotate_attrs($class, $name, $final);
@@ -544,14 +520,13 @@ if (!function_exists('class_annotate_attrs')) {
             return $attrs[count($attrs) - 1];
         }
         return $attrs;
-
+        
     }
 }
 
 
 if (!function_exists('has_process')) {
-    function has_process(int $pid): bool
-    {
+    function has_process(int $pid): bool {
         if (PHP_OS === 'WINNT') {
             $output = shell_exec("tasklist /FI \"PID eq $pid\"");
             return str_contains($output, $pid);
@@ -568,16 +543,15 @@ if (!function_exists('has_process')) {
 }
 
 if (!function_exists('call_php_script')) {
-    function call_php_script(string $path, $cmd, $logPath = null): void{
+    function call_php_script(string $path, $cmd, $logPath = null): void {
         if (PHP_OS === 'WINNT') {
-            $handle = popen("cd {$path} &&start /B $cmd > $logPath",'r');
+            $handle = popen("cd {$path} &&start /B $cmd > $logPath", 'r');
         } else {
-            $handle = popen("cd {$path} && nohup $cmd > $logPath 2>&1 &",'r');
+            $handle = popen("cd {$path} && nohup $cmd > $logPath 2>&1 &", 'r');
         }
         pclose($handle);
     }
 }
-
 
 
 if (!function_exists('transform_callable_plus')) {
@@ -615,7 +589,6 @@ if (!function_exists('transform_callable_plus')) {
 }
 
 
-
 if (!function_exists('is_callable_plus')) {
     /**
      * 判断是否为CallablePlus可执行对象
@@ -642,7 +615,7 @@ if (!function_exists('callable_plus')) {
      * @return mixed
      * @throws Exception
      */
-    function callable_plus(mixed $value,bool $validate = true): mixed {
+    function callable_plus(mixed $value, bool $validate = true): mixed {
         if ($validate && !is_callable_plus($value)) {
             throw new \Exception("Not a callable plus");
         }
@@ -652,4 +625,20 @@ if (!function_exists('callable_plus')) {
         return $call(...$value);
     }
     
+}
+
+if (!function_exists('value_compare')) {
+    function value_compare($value1, $symbol, $value2): bool {
+        return match ($symbol) {
+            '=', '==', 'equal', 'eq'        => $value1 == $value2,
+            '===', 'identical'              => $value1 === $value2,
+            '!=', '<>', 'not-equal', 'neq'  => $value1 != $value2,
+            '!==', 'not_identical'          => $value1 !== $value2,
+            '>', 'greater'                  => $value1 > $value2,
+            '<', 'less'                     => $value1 < $value2,
+            '>=', 'greater_equal'           => $value1 >= $value2,
+            '<=', 'less_equal'              => $value1 <= $value2,
+            default => throw new InvalidArgumentException("不支持的比较符号: $symbol"),
+        };
+    }
 }
